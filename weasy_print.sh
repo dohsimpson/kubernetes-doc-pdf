413add2512165634a2334926b87f57b32ecb0052
dohsimpson <mydohsimpson@gmail.com>
2019-09-22 08:18:37 -0400

    put PDF files inside PDFs/

--- weasy_print.sh
+++ weasy_print.sh
@@ -2,9 +2,11 @@
 
 set -x
 
+PDF_DIR=PDFs
+
 sed -i -e "s/’/\'/g" -e "s/–/-/g" -e "s/“/\"/g" -e "s/”/\"/g" $1.html
 
 docker run --name weasy -d rstmpw/weasyprint
 docker cp codeblock_wrap.css weasy:/tmp/codeblock_wrap.css
-docker exec -i weasy weasyprint -v -f pdf -s /tmp/codeblock_wrap.css '-' '-' >|$1.pdf <$1.html
+docker exec -i weasy weasyprint -v -f pdf -s /tmp/codeblock_wrap.css '-' '-' >| "$PDF_DIR/$1.pdf" <$1.html
 docker rm -f weasy
