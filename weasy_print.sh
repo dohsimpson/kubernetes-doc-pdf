dd1f6178a0c2cc31a6485cffb31858223f9f1b44
dohsimpson <mydohsimpson@gmail.com>
2020-01-18 14:09:15 -0500

    Fix #7

--- weasy_print.sh
+++ weasy_print.sh
@@ -6,7 +6,7 @@ PDF_DIR=PDFs
 
 sed -i -e "s/’/\'/g" -e "s/–/-/g" -e "s/“/\"/g" -e "s/”/\"/g" $1.html
 
-docker run --name weasy -d rstmpw/weasyprint
+docker run --name weasy -d rstmpw/weasyprint:46
 docker cp codeblock_wrap.css weasy:/tmp/codeblock_wrap.css
 docker exec -i weasy weasyprint -v -f pdf -s /tmp/codeblock_wrap.css '-' '-' >| "$PDF_DIR/$1.pdf" <$1.html
 docker rm -f weasy
