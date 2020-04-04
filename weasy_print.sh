2b1cbc091307857626ffc43145332240b1538854
dohsimpson <mydohsimpson@gmail.com>
2020-04-03 23:00:25 -0400

    Update 2020-04-03

--- weasy_print.sh
+++ weasy_print.sh
@@ -6,7 +6,7 @@ PDF_DIR=PDFs
 
 sed -i -e "s/’/\'/g" -e "s/–/-/g" -e "s/“/\"/g" -e "s/”/\"/g" $1.html
 
-docker run -it --name weasy -d dohsimpson/weasyprint:47
+docker run -it --name weasy -d dohsimpson/weasyprint:51
 docker cp codeblock_wrap.css weasy:/tmp/codeblock_wrap.css
 docker exec -i weasy weasyprint -v -f pdf -s /tmp/codeblock_wrap.css '-' '-' >| "$PDF_DIR/$1.pdf" <$1.html
 docker stop weasy
