73e8987aaca73793a16f945c2495d6f43395108a
dohsimpson <mydohsimpson@gmail.com>
2022-12-16 23:41:17 -0500

    v1.26

--- weasy_print.sh
+++ weasy_print.sh
@@ -8,6 +8,9 @@ sed -i -e "s/’/\'/g" -e "s/–/-/g" -e "s/“/\"/g" -e "s/”/\"/g" $1.html
 
 docker run -it --name weasy -d 4teamwork/weasyprint:56.1
 docker cp codeblock_wrap.css weasy:/tmp/codeblock_wrap.css
-docker exec -i weasy weasyprint -v -s /tmp/codeblock_wrap.css '-' '-' >| "$PDF_DIR/$1.pdf" <$1.html
+# fix weasyprint bug
+docker exec -u0 weasy sed -i "s/float(node.get('stroke-opacity', 1))/node.get('stroke-opacity', 1); stroke_opacity = 1.0 if type(stroke_opacity) == str else float(stroke_opacity)/" /usr/lib/python3.10/site-packages/weasyprint/svg/__init__.py
+docker exec -u0 weasy sed -i "s/float(node.get('fill-opacity', 1))/node.get('fill-opacity', 1); fill_opacity = 1.0 if type(fill_opacity) == str else float(fill_opacity)/" /usr/lib/python3.10/site-packages/weasyprint/svg/__init__.py
+docker exec -i weasy weasyprint -v -s /tmp/codeblock_wrap.css '-' '-' >| "$PDF_DIR/$1.pdf" < $1.html
 docker stop weasy
 docker rm -f weasy
